d8abef04680839ad699778e879e69d24b25a1e46
few1245 <139094131+few1245@users.noreply.github.com>
2023-07-10 10:39:18 +0700

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{   cout <<"student code:6606021631050"<< endl;
+    cout <<" studentname:weeraphat chuenchob"<< endl;
+    cout <<"    division:information technology"<< endl;
+    cout <<"        year:1"<< endl;
+    cout <<"        Room:Rc"<< endl;
+    cout <<"         sex:male"<< endl;
+    cout <<"         age:19"<< endl;
+  return (0);
+}
