d87c7148ded5e8615365b86604da3a80875610fc
few1245 <139094131+few1245@users.noreply.github.com>
2023-07-24 10:03:40 +0700

    Add files via upload

---
+++ rew.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      int score;
+  
+	  cout<<"input  c++ score : ";
+      cin >>score;
+	  cout<<"you"<<((score>=50)? " pass ":" fail ")<<"with score = "<<score<<endl;
+	 
+	  system("Pause");
+	  return (0);
+}
