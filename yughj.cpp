47913fe9dca6f282764fdb5395f216e0fe4de228
few1245 <139094131+few1245@users.noreply.github.com>
2023-07-30 00:21:23 +0700

    Add files via upload

---
+++ yughj.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int mt, fl;
+	float qmax, total, q1, q2, q3, ;
+	cout << "Enter final quizz(10) : ";
+	cin  >> q1;
+	cout << "Enter second  quizz(10) : ";
+	cin  >> q2;
+	cout << "Enter second  quizz(10) : ";
+	cin  >> q3;
+	cout << "Enter mid-term(40) : ";
+	cin  >> mt;
+	cout << "Enter final(50) : ";
+	cin  >> fl;
+	qmax  = ((q1 + q2 + q3) / 3);
+	total = qmax + fl + mt;
+
+	cout << "Quizz Total :  " << qmax << endl;
+	cout << "mid term : " << mt << endl;
+	cout << "Final : " << fl << endl;
+	cout << "total : " << qmax + mt + fl << endl;
+	cout << "Your score is " << ((total >= 50) ? "pass" : "fail") << endl;
+
+	system("pause");
+	return (0);
+}
